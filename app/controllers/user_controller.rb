509a5fc6d33412cba69d78ac7016ca6902099d77
rasit1 <rasitramiakatra@gmail.com>
2019-03-14 20:55:35 +0300

    first commit front end

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+ 
+  def new
+  end
+
+  def create
+
+  	puts '*' * 100
+
+  	User.create(nom: params[:Nom] , prenom: params[:Prenom] , age: params[:Age] , email: params[:Email] , password: params[:password])
+  	
+  	redirect_to new_session_path
+
+  end
+end
